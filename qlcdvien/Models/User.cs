add0bf06ea61676f3185636daff370ab47018059
Administrator <tranxuanhuy1@gmail.com>
2018-12-26 07:24:44 +0700

    tiep tuc viet hoa button, link html

--- qlcdvien/Models/User.cs
+++ qlcdvien/Models/User.cs
@@ -21,7 +21,7 @@ namespace qlcdvien.Models
 
         [Column(TypeName = "date")]
         [DisplayName("Ngày sinh")]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime? DOB { get; set; }
 
         [DisplayName("Giới tính")]
