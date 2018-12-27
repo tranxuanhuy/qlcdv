a0e7417edffdf83884e2d5027b19d64b78a69212
Administrator <tranxuanhuy1@gmail.com>
2018-12-28 06:43:56 +0700

    suas ngay sinh MM/dd/yyyy moi save khi edit user duoc, edit hoatdongCD ko hien thi noi dung

--- qlcdvien/Models/User.cs
+++ qlcdvien/Models/User.cs
@@ -21,7 +21,7 @@ namespace qlcdvien.Models
 
         [Column(TypeName = "date")]
         [DisplayName("Ngày sinh")]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime? DOB { get; set; }
 
         [DisplayName("Giới tính")]
